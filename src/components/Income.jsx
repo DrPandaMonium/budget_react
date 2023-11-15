da20ef8d75c7105171402e2a7206c349a3a8bf2b
DrPandaMonium <105460824+DrPandaMonium@users.noreply.github.com>
2023-11-14 20:26:32 -0600

    created the skeleton for the income comp

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Income = () => {
     return (
-        <>
-        </>
+        <div>
+            <div>
+                <h1>Needs: 50%</h1>
+                <div>
+                    <h2>Rent: 64%</h2>
+                    <h2>Car Payment and Insurance: 22%</h2>
+                    <h2>Groceries: 14%</h2>
+                </div>
+            </div>
+            <div>
+                <h1>Savings: 20%</h1>
+                <div>
+                    <h2>Student Debt: 44%</h2>
+                    <h2>Credit Card Payment: 34%</h2>
+                    <h2>Savings: 22%</h2>
+                </div>
+            </div>
+            <div>
+                <h1>Wants: 30%</h1>
+                <div>
+                    <h2>Cellphone Service: 12%</h2>
+                    <h2>Streaming Services: 4%</h2>
+                    <h2>Internet: 8%</h2>
+                    <h2>Dining: 47%</h2>
+                    <h2>Shopping: 29%</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
