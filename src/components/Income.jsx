b2dbb091175517aa98a89849e649fbca3d0508a1
DrPandaMonium <105460824+DrPandaMonium@users.noreply.github.com>
2023-11-28 18:51:41 -0600

    added initial input fields for input comp

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -4,29 +4,32 @@ const Income = () => {
     return (
         <div>
             <div>
-                <h1>Needs: 50%</h1>
+                <h1>Needs: 50%<input type='number'></input></h1>
                 <div>
-                    <h2>Rent: 64%</h2>
-                    <h2>Car Payment and Insurance: 22%</h2>
-                    <h2>Groceries: 14%</h2>
+                    <h2>Rent: 64%<input type='number'></input></h2>
+                    <h2>Car Payment and Insurance: 22%<input type='number'></input></h2>
+                    <h2>Groceries: 14%<input type='number'></input></h2>
+                    <h2>Other:<input type='number'></input></h2>
                 </div>
             </div>
             <div>
                 <h1>Savings: 20%</h1>
                 <div>
-                    <h2>Student Debt: 44%</h2>
-                    <h2>Credit Card Payment: 34%</h2>
-                    <h2>Savings: 22%</h2>
+                    <h2>Student Debt: 44%<input type='number'></input></h2>
+                    <h2>Credit Card Payment: 34%<input type='number'></input></h2>
+                    <h2>Savings: 22%<input type='number'></input></h2>
+                    <h2>Other:<input type='number'></input></h2>
                 </div>
             </div>
             <div>
                 <h1>Wants: 30%</h1>
                 <div>
-                    <h2>Cellphone Service: 12%</h2>
-                    <h2>Streaming Services: 4%</h2>
-                    <h2>Internet: 8%</h2>
-                    <h2>Dining: 47%</h2>
-                    <h2>Shopping: 29%</h2>
+                    <h2>Cellphone Service: 12%<input type='number'></input></h2>
+                    <h2>Streaming Services: 4%<input type='number'></input></h2>
+                    <h2>Internet: 8%<input type='number'></input></h2>
+                    <h2>Dining: 47%<input type='number'></input></h2>
+                    <h2>Shopping: 29%<input type='number'></input></h2>
+                    <h2>Other:<input type='number'></input></h2>
                 </div>
             </div>
         </div>
